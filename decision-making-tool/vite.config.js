5df4a422a79ca55f39c354694e06dac658bd849b
Alena Radomskaia <radomskaia.a.i@gmail.com>
2025-02-27 17:58:04 +0700

    chore: scaffold the project

---
+++ decision-making-tool/vite.config.js
@@ -0,0 +1,51 @@
+import path, { resolve } from "path";
+import { defineConfig } from "vite";
+import autoprefixer from "autoprefixer";
+import checker from "vite-plugin-checker";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+import createSvgSpritePlugin from "vite-plugin-svg-spriter";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+const SRC_PATH = path.resolve(__dirname, "src");
+const SVG_FOLDER_PATH = path.resolve(SRC_PATH, "shared/img/svg");
+
+export default defineConfig({
+  base: "./",
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
+    },
+    cssCodeSplit: false,
+  },
+  css: {
+    postcss: {
+      plugins: [autoprefixer()],
+    },
+  },
+  plugins: [
+    createSvgSpritePlugin({ svgFolder: SVG_FOLDER_PATH }),
+    ViteImageOptimizer({
+      jpg: {
+        quality: 85,
+      },
+      png: {
+        quality: 85,
+      },
+      webp: {
+        quality: 70,
+      },
+    }),
+    tsconfigPaths(),
+    checker({
+      typescript: true,
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
