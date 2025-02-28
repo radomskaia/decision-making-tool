e444ec43960f9dacb8266943cb98454b72c17005
Alena Radomskaia <radomskaia.a.i@gmail.com>
2025-02-28 17:54:42 +0700

    feat: implement router.ts

---
+++ decision-making-tool/src/components/router.ts
@@ -0,0 +1,36 @@
+import type { Callback } from "@/type";
+
+export class Router {
+  private routers = new Map<string, Callback>();
+  private currentPath = "";
+  private readonly baseUrl: string = window.location.href.split("#")[0];
+  constructor() {
+    window.addEventListener("hashchange", () => this.routerChange());
+    this.routerChange();
+  }
+  public add(route: string, handler: Callback): void {
+    this.routers.set(route, handler);
+  }
+  public navigateTo(path: string): void {
+    if (path === this.currentPath) {
+      return;
+    }
+    if (this.routers.has(path)) {
+      const handler = this.routers.get(path);
+      this.currentPath = path;
+      window.history.pushState(null, "", `#${path}`);
+      window.history.replaceState(null, "", `${this.baseUrl}#${path}`);
+
+      if (handler) {
+        handler();
+      }
+    } else {
+      console.error("Not Found", path);
+    }
+  }
+
+  private routerChange(): void {
+    const hash: string = window.location.hash.slice(1) || "/";
+    this.navigateTo(hash);
+  }
+}
