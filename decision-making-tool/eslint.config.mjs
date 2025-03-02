b62507eebc62f7b48ea288baf92351fa5a448f15
Alena Radomskaia <radomskaia.a.i@gmail.com>
2025-03-02 19:01:24 +0700

    chore: update configs

---
+++ decision-making-tool/eslint.config.mjs
@@ -0,0 +1,84 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
+import perfectionist from "eslint-plugin-perfectionist";
+import importPlugin from "eslint-plugin-import";
+import unicorn from "eslint-plugin-unicorn";
+import prettier from "eslint-plugin-prettier";
+
+export default [
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      globals: globals.browser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parser: tsparser,
+      parserOptions: {
+        project: ["./tsconfig.json"],
+      },
+    },
+    plugins: {
+      pluginJs,
+      "@typescript-eslint": tseslint,
+      perfectionist,
+      import: importPlugin,
+      unicorn,
+      prettier,
+    },
+    rules: {
+      ...pluginJs.configs.recommended.rules,
+      ...tseslint.configs.strict.rules,
+      ...tseslint.configs.stylistic.rules,
+      curly: ["error", "all"],
+      "max-lines-per-function": [
+        "error",
+        { max: 40, skipBlankLines: true, skipComments: true },
+      ],
+
+      "@typescript-eslint/no-inferrable-types": "error",
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/explicit-function-return-type": "error",
+      "@typescript-eslint/consistent-type-exports": "error",
+      "@typescript-eslint/consistent-type-imports": "error",
+      "import/order": ["error", { "newlines-between": "always" }],
+      "unicorn/better-regex": "error",
+      "@typescript-eslint/no-misused-promises": [
+        "error",
+        {
+          checksVoidReturn: {
+            arguments: false,
+          },
+        },
+      ],
+      "@typescript-eslint/explicit-member-accessibility": [
+        "error",
+        {
+          accessibility: "explicit",
+          overrides: {
+            constructors: "off",
+          },
+        },
+      ],
+      "@typescript-eslint/consistent-type-assertions": [
+        "error",
+        { assertionStyle: "never" },
+      ],
+    },
+    linterOptions: {
+      noInlineConfig: true,
+      reportUnusedDisableDirectives: true,
+    },
+    settings: {
+      "import/resolver": {
+        typescript: {
+          project: "./tsconfig.json",
+        },
+      },
+      "import/parsers": {
+        "@typescript-eslint/parser": [".ts"],
+      },
+    },
+  },
+];
